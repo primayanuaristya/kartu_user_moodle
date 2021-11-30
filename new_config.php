4441e656d5b1fa36631a3551922e380ca2a68b1f
primayanuaristya <primayanuaristya@gmail.com>
2021-11-30 15:12:14 +0700

    Update new_config.php

--- new_config.php
+++ new_config.php
@@ -1,19 +1,10 @@
 <?php
 require '../config.php';
 $newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,$CFG->dbpass,$CFG->dbname);
-// $newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,'123',$CFG->dbname);
-// Check connection
 
 if (mysqli_connect_errno($newConn)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
   }
 
-// $CFG->dbtype    = 'mysqli';
-// $CFG->dblibrary = 'native';
-// $CFG->dbhost    = 'localhost';
-// $CFG->dbname    = 'moo34';
-// $CFG->dbuser    = 'moo34';
-// $CFG->dbpass    = 'xyUClAuRLXLaOcod';
-// $CFG->prefix    = 'mdl_';
 ?>
